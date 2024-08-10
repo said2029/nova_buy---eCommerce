dd910e7cff937266237db6aa227b89974ca1a1cb
Saeed Bennana <115021608+SaidBennana@users.noreply.github.com>
2024-08-10 16:09:28 +0100

    loading page

--- src/app/[locale]/dashboard/laoding.tsx
+++
@@ -1,5 +0,0 @@
-export default function laoding() {
-  return (
-    <div>laoding..</div>
-  )
-}
---
+++ src/app/[locale]/dashboard/loading.tsx
@@ -0,0 +1,9 @@
+import Loadiner from "@/components/ui/Loadiner";
+
+export default function laoding() {
+  return (
+    <div className="w-full h-screen relative">
+      <Loadiner />
+    </div>
+  );
+}
